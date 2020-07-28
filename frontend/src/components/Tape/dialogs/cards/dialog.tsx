800659320b5ae52244b760115501d6884de8f1c0
pashuka <p@simdo.ru>
2020-07-28 11:21:28 +0300

    Improve dicalog card logic - get user profile from recoil store - remove inner fetchers - refactore component state

--- frontend/src/components/Tape/dialogs/cards/dialog.tsx
+++ frontend/src/components/Tape/dialogs/cards/dialog.tsx
@@ -2,73 +2,30 @@ import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import dayjs from 'dayjs';
 import IPerson from '@material-ui/icons/Person';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import isToday from 'dayjs/plugin/isToday';
-import {
-  QSParamsType,
-  ParamsKeyDialog,
-  routes,
-  apis,
-  host,
-} from '../../../../constants';
-import { useFetch } from 'react-async';
+import { QSParamsType, ParamsKeyDialog, routes } from '../../../../constants';
 import { AuthAtom } from '../../../../hooks/recoil/auth';
 import { MessengerAtom } from '../../../../hooks/recoil/messenger';
-import { UsersAtom, UserType } from '../../../../hooks/recoil/user';
+import { UserInfo } from '../../../../hooks/recoil/user';
 import { DialogType } from '../../../../hooks/recoil/dialog';
-// import { compactNumber } from '../../../../utils';
 
 dayjs.extend(isToday);
 
 type DialogPropsType = {
-  dialog?: DialogType;
+  dialog: DialogType;
 };
 
 const Dialog = ({ dialog }: DialogPropsType) => {
   const isOnline = false;
-  const date = dialog ? new Date(dialog?.last_message_created_at) : '';
-  const [participant, setParticipant] = React.useState<UserType | undefined>();
+  const date = dialog ? new Date(dialog.last_message_created_at) : '';
   const { params } = useRouteMatch<QSParamsType>();
   const [messenger, setMessenger] = useRecoilState(MessengerAtom);
   const [{ data: iam }] = useRecoilState(AuthAtom);
-  const [users, setUsers] = useRecoilState(UsersAtom);
-
-  const currentParticipant = dialog?.participants.find(
-    (_) => _ !== iam?.username,
-  );
-
-  const { data, run } = useFetch<UserType>(
-    `${host}/${apis.version}/get/${routes.user}/?username=`,
-    { headers: { accept: 'application/json' } },
-    { defer: true },
-  );
-
-  React.useEffect(() => {
-    if (currentParticipant) {
-      const record = users.data?.find(
-        ({ username }) => username === currentParticipant,
-      );
-      if (record) {
-        setParticipant(record);
-      } else {
-        run({
-          resource: `${host}/${apis.version}/get/${routes.user}/?username=${currentParticipant}`,
-        });
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  React.useEffect(() => {
-    if (data && data.username === currentParticipant) {
-      setUsers((prev) =>
-        prev.data ? { ...prev, data: [...prev.data, data] } : prev,
-      );
-      setParticipant(data);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
+  const participantName =
+    dialog.participants.find((_) => _ !== iam?.username) || '';
+  const participant = useRecoilValue(UserInfo(participantName));
 
   let sDate;
   if (dayjs(date).isToday()) {
@@ -81,14 +38,14 @@ const Dialog = ({ dialog }: DialogPropsType) => {
   return (
     <Link
       className="nav-link text-body p-0 border-bottom m-2"
-      to={`/${routes.dialogs}/${dialog?.dialog_id}/`}
+      to={`/${routes.dialogs}/${dialog.dialog_id}/`}
       onClick={(e) => {
         setMessenger({ isOpen: messenger.isOpen, isChatOpen: true });
       }}
     >
       <div
         className={`card border-0 rounded-0 ${
-          dialog?.dialog_id === params[ParamsKeyDialog]
+          dialog.dialog_id === params[ParamsKeyDialog]
             ? 'alert-primary'
             : 'card-regular'
         }`}
@@ -115,13 +72,15 @@ const Dialog = ({ dialog }: DialogPropsType) => {
             <div className="media-body overflow-hidden">
               <div className="d-flex align-items-center">
                 <h6 className="text-truncate mb-0 mr-auto">
-                  {participant?.realname || participant?.username}
+                  {participant?.realname || (
+                    <span>@{participant?.username || participantName}</span>
+                  )}
                 </h6>
                 <p className="small text-muted text-nowrap ml-4">{sDate}</p>
               </div>
               <div className="d-flex align-items-center">
                 <div className="text-muted text-truncate text-left mr-auto">
-                  {dialog?.last_message_body}
+                  {dialog.last_message_body}
                 </div>
                 {/* <div className="badge badge-pill badge-primary ml-3">
                   {compactNumber(9)}

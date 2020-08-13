dfa02ab5d19a9fa9125e37efb3b1f3ec11136891
pashuka <p@simdo.ru>
2020-08-13 08:08:40 +0300

    recoil.messages.fetch with offset

--- frontend/src/hooks/recoil/message.ts
+++ frontend/src/hooks/recoil/message.ts
@@ -2,6 +2,7 @@ import Recoil from 'recoil';
 import { request } from './request';
 import { DialogIdType, currentDialogIdState } from './dialog';
 import { routes, getRoute } from '../../constants';
+import { limitFetchMax } from './constants';
 
 export type MessageType = {
   dialog_id: DialogIdType;
@@ -10,30 +11,43 @@ export type MessageType = {
   body: string;
 };
 
-export type MessagesType = {
-  [key: string]: MessageType[];
-};
-
 const atomTrigger = Recoil.atom({
   key: 'messagesTrigger',
   default: 0,
 });
 
-export const messagesState = Recoil.selector<MessageType[] | undefined>({
-  key: 'messagesState',
-  get: async ({ get }) => {
-    get(atomTrigger); // 'register' as a dependency
+export const messagesOffsetAtom = Recoil.atom<number>({
+  key: 'messagesOffsetAtom',
+  default: 0,
+});
+
+export const messagesByOffset = Recoil.selectorFamily<MessageType[], number>({
+  key: 'messagesByOffset',
+  get: (offset) => async ({ get }) => {
     const id = get(currentDialogIdState);
     return id
       ? await request<MessageType[]>(
-          getRoute(`find/${routes.messages}/?dialog_id=${id}`),
+          getRoute(`find/${routes.messages}/?dialog_id=${id}&offset=${offset}`),
         ).then(
-          (data) => data,
+          (data) => (Array.isArray(data) ? data : ([] as MessageType[])),
           (reason) => {
             throw reason;
           },
         )
-      : undefined;
+      : ([] as MessageType[]);
+  },
+});
+
+export const messagesState = Recoil.selector<MessageType[]>({
+  key: 'messagesState',
+  get: async ({ get }) => {
+    get(atomTrigger); // 'register' as a resetable dependency
+    const offset = get(messagesOffsetAtom);
+    let records = [] as MessageType[];
+    for (let index = 0; index <= offset; index += limitFetchMax) {
+      records = records.concat(get(messagesByOffset(index)));
+    }
+    return records;
   },
   set: ({ set }, value) => {
     if (value instanceof Recoil.DefaultValue) {
